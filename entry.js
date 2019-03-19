4699a401add4e7deb18a2798fe8d2eb1185d134b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-03-19 10:16:02 -0400

    Use semi-colons consistently

--- entry.js
+++ entry.js
@@ -3,7 +3,7 @@ var jsonmergepatch = require('json-merge-patch');
 var JSZip = require("jszip");
 
 var metaschema = require('json!./metaschema.json');
-var file_name = 'release-schema.json'
+var file_name = 'release-schema.json';
 
 var schemas = {
   'release-schema-1.0': require('json!./release-schema/release-schema-1.0.json'),
@@ -12,7 +12,7 @@ var schemas = {
   'release-package-schema-1.1': require('json!./release-package-schema/release-package-schema-1.1.json'),
   'record-package-schema-1.0': require('json!./record-package-schema/record-package-schema-1.0.json'),
   'record-package-schema-1.1': require('json!./record-package-schema/record-package-schema-1.1.json')
-}
+};
 
 
 var original = document.getElementById('jsoneditor-original');
@@ -31,7 +31,7 @@ FileReaderJS.setupInput(document.getElementById('upload-original'), {
   readAsDefault: 'Text',
   on: {
     load: function (event, file) {
-      file_name = file.name
+      file_name = file.name;
       editor_original.setText(event.target.result);
     }
   }
@@ -40,8 +40,8 @@ document.getElementById('select-original').onchange = function (event) {
   editor_original.set(schemas[event.target.value]);
   editor_target.setText('{}');
   editor_patch.setText('{}');
-  file_name = event.target.value.replace(event.target.value.substr(-4), '.json')
-}
+  file_name = event.target.value.replace(event.target.value.substr(-4), '.json');
+};
 
 var target = document.getElementById('jsoneditor-target');
 var options_target = {
@@ -68,7 +68,7 @@ FileReaderJS.setupInput(document.getElementById('upload-target'), {
 
 document.getElementById('copy-target').onclick = function (event) {
   editor_target.set(editor_original.get());
-}
+};
 
 
 var patch = document.getElementById('jsoneditor-patch');
@@ -81,9 +81,9 @@ var options_patch = {
 var editor_patch = new JSONEditor(patch, options_patch, {});
 
 document.getElementById('generate-patch').onclick = function (event) {
-  var generated_patch = jsonmergepatch.generate(editor_original.get(), editor_target.get())
+  var generated_patch = jsonmergepatch.generate(editor_original.get(), editor_target.get());
   if (!generated_patch) {
-    generated_patch = {}
+    generated_patch = {};
   }
   editor_patch.set(generated_patch);
 }
@@ -92,7 +92,7 @@ document.getElementById('generate-patch').onclick = function (event) {
 document.getElementById('download-patch').onclick = function () {
   var patch = editor_patch.getText();
   if (patch !== '{}') {
-    var json_object = JSON.parse(patch)
+    var json_object = JSON.parse(patch);
     var blob = new Blob([JSON.stringify(json_object, null, 2)], {type: 'application/json;charset=utf-8'});
     saveAs(blob, file_name);
   }
@@ -105,49 +105,48 @@ document.getElementById('generate-extension').onclick = function () {
 
   // Follow convention by requiring exactly two slashes after http(s):  
   var urlStartPattern = new RegExp('^https?:\/\/[^\/\\s][^\\s]+$', 'i');
-  
+
   if (documentationUrl) {
     documentationUrl = documentationUrl.replace('\\r', '').replace('\\n', '');
     documentationUrl = documentationUrl.trim();
     if (!urlStartPattern.test(documentationUrl)) {
-      var explanation =  ' is not a valid URL.'
+      var explanation =  ' is not a valid URL.';
       if (documentationUrl.indexOf('http') === -1){
         explanation += ' Make sure that it starts with "http://" or "https://"';
       }
       alert('"' + documentationUrl + '"' + explanation);
-      return false
+      return false;
     }
   }
 
   if (!name || !description) {
-    return true
+    return true;
   }
 
-  var generated_patch = jsonmergepatch.generate(editor_original.get(), editor_target.get())
+  var generated_patch = jsonmergepatch.generate(editor_original.get(), editor_target.get());
   var zip = new JSZip();
   var patch_files = {
     'release-schema.json': '{}',
     'release-package-schema.json': '{}',
-    'record-package-schema.json': '{}',
-  }
+    'record-package-schema.json': '{}'
+  };
   patch_files[file_name] = JSON.stringify(generated_patch, null, 2);
 
   Object.keys(patch_files).forEach(function(key) {
     if (patch_files[key] != '{}') {
-      zip.file(key, patch_files[key])
+      zip.file(key, patch_files[key]);
     }
-  })
+  });
 
-  zip.file('README.md', description)
+  zip.file('README.md', description);
   zip.file('extension.json', JSON.stringify({
     'name': {'en': name},
     'description': {'en': description},
     'documentationUrl': {'en': documentationUrl}
-  }, null, 2))
-  zip.generateAsync({type: 'blob'})
-    .then(function(content) {
-        saveAs(content, name + '.zip');
-    });
+  }, null, 2));
+  zip.generateAsync({type: 'blob'}).then(function(content) {
+    saveAs(content, name + '.zip');
+  });
 
   return false;
 
